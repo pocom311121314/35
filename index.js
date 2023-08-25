09788ff060af1bb44f2794741520e2aa1f894950
pocom311121314 <pocom311121314@gmail.com>
2023-08-25 22:28:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,97 @@
+
+
+/**********product section***************/
+import ProductData from "./indexdb.js"
+
+const category = [...new Set(ProductData.map(el => el.category))]
+console.log(category)
+
+
+let filterData = []
+
+document.addEventListener("click", (e) => {
+
+
+    const bluetoothEl = document.getElementById("bluetooth").checked
+    const ChainsEl = document.getElementById("chains").checked
+    const KurtasEl = document.getElementById("kurtas").checked
+    const AccessoriesEl = document.getElementById("accessories").checked
+    const sareesEl = document.getElementById("sarees").checked
+    const watchEl = document.getElementById("watch").checked
+
+    console.log(bluetoothEl)
+    filterData = ProductData.filter(el => (
+        bluetoothEl && el.category == "bluetooth Headset" ||
+        ChainsEl && el.category == "Men Chains" ||
+        KurtasEl && el.category == "Kurtas" ||
+        AccessoriesEl && el.category == "Mobile Accessories" ||
+        sareesEl && el.category == "sarees" ||
+        watchEl && el.category == "watch"
+    ))
+
+    renderProductData()
+
+
+})
+
+function renderProductData(){
+    let filterDataHTML = "";
+
+    if(filterData[0]){
+        filterData.forEach(el => {
+            filterDataHTML += `
+            <div class="productCard" onclick="ClickProduct(${el.id})">
+                <div class="product_image">
+                    <img src="./${el.image}"/>
+                </div>
+                <h3 class="product_name">${el.title}</h3>
+                <p class="product_price">
+                    <span>₹</span>
+                    <span>${el.price}</span>
+                </p>
+             </div>
+            `
+        })
+    }
+    else{
+        ProductData.forEach(el => {
+            filterDataHTML += `
+            <div class="productCard" onclick="ClickProduct(${el.id})">
+                <div class="product_image">
+                    <img src="./${el.image}"/>
+                </div>
+                <h3 class="product_name">${el.title}</h3>
+                <h4 class="product_name">${el.category}</h4>
+                <p class="product_price"><span>₹</span><span>${el.price}</span></p>
+                <a href= ${el.purchase} ><h4>BUY NOW</h4></a>
+                
+                </div>
+            `
+        }) 
+    }
+    
+    document.getElementById("product_category_displayId").innerHTML = filterDataHTML
+} 
+renderProductData()
+
+
+
+function ClickProduct(id){
+    localStorage.setItem("productId",JSON.stringify(id))
+    // window.location("./page/product.html")
+
+    alert("hii")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ search.js
@@ -0,0 +1,353 @@
+let products = {
+  data: [
+
+    {
+      title : "BOAT AIRPODS PRO (BLACK)",
+      category: "HEADPHONE",
+      price : "200",
+      image : "static/01.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_BOAT_AIRPODS_PRO_(BLACK)_₹200",
+  },
+  {
+      title : "BOAT AIRPODS PRO (GREEN-WHITE)",
+      category: "HEADPHONE",
+      price : "250",
+      image : "static/02.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_BOAT_AIRPODS_PRO_(GREEN-WHITE)_₹250",
+  },
+  {
+      title : "BOAT AIRPODS PRO (RED)",
+      category: "HEADPHONE",
+      price : "300",
+      image : "static/03.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_BOAT_AIRPODS_PRO_(RED)_₹300",
+  },
+  {
+      title : "BOAT AIRPODS PRO (GREEN)",
+      category: "HEADPHONE",
+      price : "350",
+      image : "static/04.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_BOAT_AIRPODS_PRO_(GREEN)_₹350",
+  },
+  {
+      title : "BOAT AIRPODS WIRELESS (BLUE)",
+      category: "HEADPHONE",
+      price : "400",
+      image : "static/05.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_BOAT_AIRPODS_WIRELESS_(BLUE)_₹400",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/06.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/07.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/08.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/09.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/10.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/11.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/12.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/13.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/14.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/15.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/16.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/17.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/18.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/19.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/20.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/21.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/22.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/23.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/24.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/25.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/26.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/27.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/28.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/29.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/30.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/31.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "boAt New AirPods Pro",
+      category: "HEADPHONE",
+      price : "346",
+      image : "static/32.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_sony_cx239",
+  },
+  {
+      title : "HP GAMING VICTUS",
+      category: "LAPTOP",
+      price : "25,000",
+      image : "static/33.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_HP_GAMING_VICTUS_₹25,000",
+  },
+  {
+      title : "HP GAMING VICTUS PRO",
+      category: "LAPTOP",
+      price : "65,000",
+      image : "static/34.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_HP_GAMING_VICTUS_PRO_₹65,000",
+  },
+  {
+      title : "HP GAMING VICTUS ROG",
+      category: "LAPTOP",
+      price : "85,000",
+      image : "static/35.webp",
+      purchase : "https://api.whatsapp.com/send?phone=919679432975&text=i_want_to_buy_HP_GAMING_VICTUS_ROG_₹85,000",
+  },
+ 
+
+  ],
+};
+
+for (let i of products.data) {
+  //Create Card
+  let card = document.createElement("div");
+  //Card should have category and should stay hidden initially
+  card.classList.add("card", i.category, "hide");
+  //image div
+  let imgContainer = document.createElement("div");
+  imgContainer.classList.add("image-container");
+  //img tag
+  let image = document.createElement("img");
+  image.setAttribute("src", i.image);
+  imgContainer.appendChild(image);
+  card.appendChild(imgContainer);
+  //container
+  let container = document.createElement("div");
+  container.classList.add("container");
+  //product id
+  
+  //product title
+  let title = document.createElement("h5");
+  title.classList.add("product-title");
+  title.innerText = i.title.toUpperCase();
+  container.appendChild(title);
+  // product category
+  let category = document.createElement("h6");
+  category.innerText = i.category.toUpperCase();
+  container.appendChild(category);
+
+  //price
+  let price = document.createElement("h6");
+  price.innerText = "₹ " + i.price;
+  container.appendChild(price);
+  //purchase link
+  let purchase = document.createElement("a");
+  purchase.setAttribute("href", i.purchase);
+  purchase.innerText = "BUY NOW" ;
+  container.appendChild(purchase);
+
+  card.appendChild(container);
+  document.getElementById("products").appendChild(card);
+}
+
+//parameter passed from button (Parameter same as category)
+function filterProduct(value) {
+  //Button class code
+  let buttons = document.querySelectorAll(".button-value");
+  buttons.forEach((button) => {
+    //check if value equals innerText
+    if (value.toUpperCase() == button.innerText.toUpperCase()) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+
+  //select all cards
+  let elements = document.querySelectorAll(".card");
+  //loop through all cards
+  elements.forEach((element) => {
+    //display all cards on 'all' button click
+    if (value == "all") {
+      element.classList.remove("hide");
+    } else {
+      //Check if element contains category class
+      if (element.classList.contains(value)) {
+        //display element based on category
+        element.classList.remove("hide");
+      } else {
+        //hide other elements
+        element.classList.add("hide");
+      }
+    }
+  });
+}
+
+//Search button click
+document.getElementById("search").addEventListener("click", () => {
+  //initializations
+  let searchInput = document.getElementById("search-input").value;
+  let elements = document.querySelectorAll(".product-title");
+  
+  let cards = document.querySelectorAll(".card");
+
+  //loop through all elements
+  elements.forEach((element, index) => {
+    //check if text includes the search value
+    if (element.innerText.includes(searchInput.toUpperCase())) {
+      //display matching card
+      cards[index].classList.remove("hide");
+    } else {
+      //hide others
+      cards[index].classList.add("hide");
+    }
+  });
+});
+
+//Initially display all products
+window.onload = () => {
+  filterProduct("all");
+};
